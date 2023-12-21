ccae140e33adf1c696b17f25a049c52377facf64
V.Sazonov <VitaliySazonov20@gmail.com>
2023-12-21 14:08:21 +0400

    finished game

--- src/Cell.java
+++ src/Cell.java
@@ -1,2 +1,62 @@
-package PACKAGE_NAME;public class Cell {
+import javax.swing.*;
+import java.util.Objects;
+
+public class Cell extends JButton {
+    JButton button= new JButton();
+    GameWindow window;
+    int cellSize=100;
+    int x;
+    int y;
+    Cell[][] cellArray;
+    Cell(GameWindow window,int y,int x){
+        this.window=window;
+        this.x=x;
+        this.y=y;
+        button.setBounds(x*cellSize+100,y*cellSize+100,cellSize,cellSize);
+        if(4*y+x+1!=16)
+            button.setText(String.valueOf(4*y+x+1));
+        else {
+            button.setText(" ");
+            window.emptyCellX=x;
+            window.emptyCellY=y;
+        }
+        button.addActionListener(e -> {
+            move();
+        });
+        window.window.add(button);
+    }
+    public void addArray(Cell[][] cells) {
+        this.cellArray=cells;
+    }
+
+    public void move() {
+        if(x+1<4&& cellArray[y][x+1].button.getText()==" "){
+            cellArray[y][x+1].button.setText(button.getText());
+            button.setText(" ");
+            window.emptyCellX=x;
+            window.emptyCellY=y;
+
+        }
+        if(x-1>=0&& cellArray[y][x-1].button.getText()==" "){
+            cellArray[y][x-1].button.setText(button.getText());
+            button.setText(" ");
+            window.emptyCellX=x;
+            window.emptyCellY=y;
+        }
+        if(y+1<4&& cellArray[y+1][x].button.getText()==" "){
+            cellArray[y+1][x].button.setText(button.getText());
+            button.setText(" ");
+            window.emptyCellX=x;
+            window.emptyCellY=y;
+        }
+        if(y-1>=0&& cellArray[y-1][x].button.getText()==" "){
+            cellArray[y-1][x].button.setText(button.getText());
+            button.setText(" ");
+            window.emptyCellX=x;
+            window.emptyCellY=y;
+        }
+        window.check();
+    }
+
+
 }
--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,2 +1,83 @@
-package PACKAGE_NAME;public class GameWindow {
+import javax.swing.*;
+import java.util.Objects;
+import java.util.Random;
+
+public class GameWindow {
+    JFrame window = new JFrame();
+    int WIDTH = 600;
+    int HEIGHT = 600;
+    int emptyCellX;
+    int emptyCellY;
+
+    Cell[][] cells = new Cell[4][4];
+
+    GameWindow() {
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                cells[i][j] = new Cell(this, i, j);
+
+                window.add(cells[i][j]);
+            }
+        }
+        for (Cell[] cellRow : cells) {
+            for (Cell cell : cellRow) {
+                cell.addArray(cells);
+            }
+        }
+        shuffle();
+        for (Cell[] cellRow : cells) {
+            for (Cell cell : cellRow) {
+                cell.button.setEnabled(true);
+            }
+        }
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(WIDTH, HEIGHT);
+        window.setLayout(null);
+        window.setVisible(true);
+    }
+
+    private void shuffle() {
+        Random random = new Random();
+        int state;
+        for (int i = 0; i < 100; i++) {
+            state = random.nextInt(4);
+            if (state == 0 && emptyCellX + 1 < 4) {
+                cells[emptyCellY][emptyCellX + 1].move();
+            }
+            if (state == 1 && emptyCellX - 1 >= 0) {
+                cells[emptyCellY][emptyCellX - 1].move();
+            }
+            if (state == 2 && emptyCellY + 1 < 4) {
+                cells[emptyCellY + 1][emptyCellX].move();
+            }
+            if (state == 3 && emptyCellY - 1 >= 0) {
+                cells[emptyCellY - 1][emptyCellX].move();
+            }
+
+        }
+    }
+
+    public void check() {
+        boolean gameOver = true;
+        outerLoop:
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                if(i==3 && j==3)
+                    continue;
+                if (!cells[i][j].button.getText().equals(String.valueOf((4 * i) + j + 1))) {
+                    System.out.println(i+" and "+ j+ " not in right place");
+                    gameOver = false;
+                    break outerLoop;
+                }
+            }
+        }
+        if (gameOver) {
+            for (int i = 0; i < cells.length; i++) {
+                for (int j = 0; j < cells[i].length; j++) {
+                    cells[i][j].button.setEnabled(false);
+                }
+            }
+        }
+    }
 }
